dcc452193259fa1152570a61c06e8c58f98f9cfd
aksingh2156 <aksingh2156@gmail.com>
2024-07-23 12:41:55 +0530

    conditional program in java

---
+++ condition.java
@@ -0,0 +1,13 @@
+package programmingREPO;
+
+public class condition {
+    public static void main(String[] args){
+        int age = 28;
+        if (age>18 ){
+            System.out.println("Applicable for voting");
+        }
+        else {
+            System.out.println("Not Applicable for voting");
+        }
+    }
+}
