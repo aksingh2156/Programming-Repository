479a27de7707238b78a2571ccda9a8168015967b
aksingh2156 <aksingh2156@gmail.com>
2024-07-23 13:02:39 +0530

    programs

---
+++ sum.java
@@ -0,0 +1,10 @@
+package programmingREPO;
+
+public class sum {
+    public static void main(String[] args){
+        int a = 5;
+        float b = 7.5f;
+        float sum = a+b;
+    System.out.println("sum is" + sum); 
+    }
+}
---
+++ ternary_operator.java
@@ -0,0 +1,9 @@
+package programmingREPO;
+
+public class ternary_operator {
+    public static void main(String[] args){
+        int a = 34;
+        String result=(a>=18)? "eligible":"not eligible";
+        System.out.println(result);
+    }
+}
